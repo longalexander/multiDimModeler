b102835cdf40ecd209addc53a97a44d7e89bec8c
longalexander <alexlongme@hotmail.com>
2020-02-11 13:55:13 -0800

    remove elem 4

--- src/initial-data/index.js
+++ src/initial-data/index.js
@@ -1,15 +1,14 @@
 const initialData = {
   elems: {
-    'elem-1': { id:'elem-1', content:'take out the garbage' },
+    'elem-1': { id:'elem-1', content:'move trash' },
     'elem-2': { id:'elem-2', content:'watch show' },
     'elem-3': { id:'elem-3', content:'clean toilet' },
-    'elem-4': { id:'elem-4', content:'make food' }
   },
   rowColMarks: {
     'rowColMark-1': {
       id:'rowColMark-1',
       title:'Row',
-      elemIds:['elem-1', 'elem-2', 'elem-3', 'elem-4']
+      elemIds:['elem-1', 'elem-2', 'elem-3']
     },
     'rowColMark-2': {
       id:'rowColMark-2',
