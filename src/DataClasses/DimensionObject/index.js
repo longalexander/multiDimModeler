66252904b3e771c5b90d06d2cc5d50060a6d92c3
longalexander <alexlongme@hotmail.com>
2020-02-17 12:16:53 -0800

    initialized measure and dimension data classes

---
+++ src/DataClasses/DimensionObject/index.js
@@ -0,0 +1,13 @@
+class DimensionObject {
+  constructor(id, name, itemList, parentList, childList){
+    this.id = id; // unique id of this dimension
+    this.name = name; // name as displayed in the application e.g. "Quarter"
+    
+    this.item = itemList; //list of items under this dimension e.g. "[Barbie, Action Man]"
+    this.parent = parentList; // list of parent dimensions e.g. "[year, 5-year, all-time]"
+    this.child = childList; // list of child dimensions e.g. "[month, week, day]"
+
+  }
+}
+
+export default DimensionObject;
