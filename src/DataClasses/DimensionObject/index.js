d51d21d71bbfee19442025ab48a6a5524c014f2c
longalexander <alexlongme@hotmail.com>
2020-02-18 10:57:53 -0800

    edited properties in dimension object

--- src/DataClasses/DimensionObject/index.js
+++ src/DataClasses/DimensionObject/index.js
@@ -1,12 +1,11 @@
 class DimensionObject {
-  constructor(id, name, itemList, parentList, childList){
+  constructor(id, title, itemList, parentList, childList){
     this.id = id; // unique id of this dimension
-    this.name = name; // name as displayed in the application e.g. "Quarter"
+    this.title = title; // title as displayed in the application e.g. "Quarter"
 
     this.item = itemList; //list of items under this dimension e.g. "[Barbie, Action Man]"
     this.parent = parentList; // list of parent dimensions e.g. "[year, 5-year, all-time]"
     this.child = childList; // list of child dimensions e.g. "[month, week, day]"
-
   }
 }
 
